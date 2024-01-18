8e96e5abe9db928052e18b8910549a2f6d77142c
Даня Федоренко <danilgrey15@gmail.com>
2024-01-18 22:22:09 +0200

    Нічого важливого

--- Python1.py
+++ Python1.py
@@ -1,1 +1,10 @@
-print("9")
+
+main={'imya':['maks','oleg'],
+   'rost':['180','134'],
+   'ves':['90'],
+   'age':['12']}
+for key, value in main.items():
+    if '12' in value:
+        print("stoy")
+    else:
+        print('vse ok')
