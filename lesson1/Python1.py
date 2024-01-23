6ff71b1fb67bd397710a114f4f8b3eb80de17bed
ds0903 <danilgrey15@gmail.com>
2024-01-23 21:54:45 +0200

    0

--- lesson1/Python1.py
+++ lesson1/Python1.py
@@ -1,4 +1,10 @@
-main = {"imya": ["maks", "oleg"], "rost": ["180", "134"], "ves": ["90"], "age": ["12"]}
+main = {
+    "imya": ["maks", "oleg"],
+    "rost": ["180", "134"],
+    "ves": ["90"],
+    "age": ["12"],
+}
+
 for key, value in main.items():
     if "12" in value:
         print("stoy")
