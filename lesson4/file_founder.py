5386d0e6f588c6e079360a04e192bbdb314f1796
ds0903 <danilgrey15@gmail.com>
2024-01-28 15:32:23 +0200

    file_search

---
+++ lesson4/file_founder.py
@@ -0,0 +1,10 @@
+with open(file="rockyou.txt", mode="rt",encoding="LATIN-1") as file1: 
+
+    result=0    # Змінна що відповідає за кількість схожих елементів 
+    lines_search=input('Введіть будьласка строчку для пошуку ') 
+    for search in file1:
+        if lines_search == search.strip():  # Функція що перевіряє чи є ця строка в файлі
+            result +=1
+            print(f'знайдено {result} схожих файлів')
+    if result==0:   
+        print('Нічого не знайдено(')
