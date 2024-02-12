c727ea4e004573a5d78d8f6d880b15746f599d96
ds0903 <danilgrey15@gmail.com>
2024-02-12 12:29:56 +0200

    fix franks

--- lesson9/franks.py
+++ lesson9/franks.py
@@ -1,71 +1,37 @@
 
-currency_exchange ={
-    "currency_USD":{"USD":1.14}, 
-    "currency_UAH":{"UAH":42.99}, #Список курсів наших валют в CHF(Швейцарский франк)
-    "currency_EUR":{"EUR":1.06},  
-    "currency_CHF":{"CHF":1} 
-    
+currency_exchange_base1 ={
+    # "USD":1.14, 
+    # "UAH":42.99, #Список курсів наших валют в CHF(Швейцарский франк)
+    # "EUR":1.06,  
+    "CHF":2
 
 }
-
-class calc: 
-    def __init__(self, value: int, currency: str, currency_exchange) -> None:
-      self.value: int = value
-      self.currency: str = currency
-      self.currency = currency_exchange
-
-    def __truediv__(self,other):
-        """Переводимо всі наші валюти в Швейцарский франк"""
-        if self.currency == "UAH":
-            for key,value_U in currency_exchange.items():
-                if key == "currency_UAH":
-                    for value_UAH in value_U.values():
-                       return self.value / value_UAH
-        if self.currency == "USD":
-            for key,value_US in currency_exchange.items():
-                if key == "currency_USD":
-                    for value_USD in value_US.values():
-                       return  self.value / value_USD
-        if self.currency == "EUR":
-            for key,value_EU in currency_exchange.items():
-                if key == "currency_EUR":
-                    for value_EUR in value_EU.values():
-                       return  self.value / value_EUR
-        if self.currency == "CHF":
-            for key,value_CH in currency_exchange.items():
-                if key == "currency_CHF":
-                    for value_CHF in value_CH.values():
-                       return self.value / value_CHF        
-        else:   
-            raise TypeError("ERROR!!! not supported currency")             
-
-class Price(calc):
+       
+class Price:
     def __init__(self, value: int, currency: str) -> None:
-      super().__init__(value, currency, None)
       self.value: int = value
       self.currency: str = currency
-
-    """Тепер додаємо та віднімаємо вже в Швейцарский франках"""
+      self.currency_base = currency_exchange_base1
+        
 
     def __add__(self,other) -> 'Price':
+        if self.currency == other.currency:
+            return f"Сумма {self.value + other.value}, В валюті {self.currency}"
+        elif self.currency != other.currency:        
+            for key,value_CH in currency_exchange_base1.items():
+                #if key == "CHF": 
+                    return f"Сума переведених в Франки валют {self.value / value_CH + other.value / value_CH} ФРАНКІВ"
 
-        return self.value + other.value, self.currency
 
     def __sub__(self,other) -> 'Price':
-        return self.value - other.value, self.currency
+        if self.currency == other.currency:
+            return f"Різниця {self.value - other.value}, В валюті {self.currency}"
+        elif self.currency != other.currency:        
+            for key,value_CH in currency_exchange_base1.items():
+                #if key == "CHF": 
+                    return f"Різниця переведених в Франки валют {self.value / value_CH - other.value / value_CH} ФРАНКІВ"
 
-
-flight = Price(2000, "USD")
+flight = Price(2000, "UAH")
 hotel = Price(1000, "UAH")
-taxi = Price(100,"EUR")
-
-divided_flight = flight / flight
-divided_hotel = hotel / hotel   #Проводимо операцію з перерахування всіх валют в Швейцарські франки
-divided_taxi = taxi / taxi
-
-result_plus = divided_flight + divided_hotel + divided_taxi #тепер додаємо та віднімаємо наші результати в Швейцарських франках
-result_minus = divided_flight - divided_hotel - divided_taxi
-
-print(f"Результат переведення валют до Швейцарського франка:\n(Квитки на літак) {divided_flight}\n(Номер в готелі) {divided_hotel}\n(Ціна на таксі) {divided_taxi}")
-print(f"Результат додавання всіх послуг {result_plus}\nА також результат віднімання {result_minus}")
-print("ЦІНИ ВКАЗАНІ В ШВЕЙЦАРСЬКИХ ФРАНКАХ!")
+result = flight+hotel
+print(result)
