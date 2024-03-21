82f84e15f02c382f787dabb31048a9dcdc333469
ds0903 <danilgrey15@gmail.com>
2024-03-21 21:59:26 +0200

    🐛 fix time chrcking

--- lesson13/server.py
+++ lesson13/server.py
@@ -1,3 +1,4 @@
+import time
 import httpx
 import random
 import string
@@ -6,6 +7,9 @@ from typing import Callable
 import requests
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+cached_data = ""
+last_request_time = 0
 
 app = FastAPI()
 
@@ -32,21 +36,46 @@ async def get_information():
     }
 
 
-@app.post("/fetch-market")
+@app.post("/fetch-market")\
+
 def get_current_market_state(payload: dict):
+    global cached_data, last_request_time
     source = payload.get("source")
     destination = payload.get("destination")
 
-    url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={source}&to_currency={destination}&apikey=V2V43QAQ8RILGBOW"
+    url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={source}&to_currency={destination}&apikey=demo"
         # responce = requests.get(url) 
-    response = requests.get(url)
 
-    data = response.json()
-    rate = data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]
-    
-    return {"rate": rate}
+
+    current_time = time.time()
+    if current_time - last_request_time >= 10:
+        response = requests.get(url)
+        data = response.json()
+        rate = data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]
+        current_time = time.time()
+        cached_data = rate
+        last_request_time = current_time
+        return {cached_data}
+    else:
+        return {cached_data}
 
         
 
         #http://localhost:8000/fetch-market?source=EUR&destination=USD
-        #http://localhost:8000/fetch-market
+        #http://localhost:8000/fetch-market 
+    
+    # """Response example:
+    #    {
+    #     "Realtime Currency Exchange Rate": {
+    #         "1. From_Currency Code": "UAH",
+    #         "2. From_Currency Name": "Ukrainian Hryvnia",
+    #         "3. To_Currency Code": "USD",
+    #         "4. To_Currency Name": "United States Dollar",
+    #         "5. Exchange Rate": "0.02610000",
+    #         "6. Last Refreshed": "2024-03-07 17:45:47",
+    #         "7. Time Zone": "UTC",
+    #         "8. Bid Price": "0.02609000",
+    #         "9. Ask Price": "0.02610000"
+    #     }
+    # }
+    # """
